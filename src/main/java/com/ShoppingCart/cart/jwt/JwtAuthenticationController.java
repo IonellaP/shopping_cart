9b558b4d528cc2c26a29ec8c13a8f03b3a6162ae
devDesignMD <ionelapb27@gmail.com>
2024-06-18 02:49:11 +0300

    Jwt registration

--- src/main/java/com/ShoppingCart/cart/jwt/JwtAuthenticationController.java
+++ src/main/java/com/ShoppingCart/cart/jwt/JwtAuthenticationController.java
@@ -1,6 +1,6 @@
 package com.ShoppingCart.cart.jwt;
 
-import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.*;
 public class JwtAuthenticationController {
 
     private AuthenticationManager authenticationManager;
+
 
     private JwtTokenUtil jwtTokenUtil;
 
--- src/main/java/com/ShoppingCart/cart/jwt/SecurityConfig.java
+++ src/main/java/com/ShoppingCart/cart/jwt/SecurityConfig.java
@@ -1,39 +1,31 @@
 package com.ShoppingCart.cart.jwt;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
-    private UserDetailsService userDetailsService;
-
-    private PasswordEncoder passwordEncoder;
-
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .authorizeRequests(auth -> {
-                    auth
-                            .requestMatchers( "/login", "/register").permitAll()
-                            .anyRequest().authenticated();
-                });
+                .csrf(AbstractHttpConfigurer::disable) // Dezactivăm CSRF
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/authenticate", "/login", "/register").permitAll()
+                        .anyRequest().authenticated())
+                .formLogin(form -> form
+                        .loginPage("/login")
+                        .defaultSuccessUrl("/home", true)
+                        .permitAll())
+                .httpBasic(withDefaults());
 
         return http.build();
-    }
-
-    @Bean
-    public AuthenticationManager authenticationManager(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder);
-        return auth.build();
     }
 }
